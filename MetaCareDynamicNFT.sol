a3f454d2f2afaf556403f951749eb5a7b74047bf
N0E-P <pecci.noe@gmail.com>
2022-11-03 11:04:39 +0100

    need to compile and test it

--- MetaCareDynamicNFT.sol
+++ MetaCareDynamicNFT.sol
@@ -1,9 +1,34 @@
+// SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.12;
 
-//error list
+/* This is just an example contract to have an idea of how it could work.
+
+TO IMPROVE:
+-Compile
+
+-Test
+
+-Create basic NFT samples
+
+-Send data from chainlink
+
+-The data isn't private, but we can do it without storing data onchain too. 
+By only storing a private link to access the data offchain (Like Ocean Protocol for example)
+
+-Don't allow anybody to call the changeUserHeartRate function.
+
+-Make an Array of Doctors, do not store only one
+
+-We can add other health data in the userData struct
+(surname, name, age, weight, height...)
+*/
+
 error YouAlreadyHaveAnNFT();
-error ThisTokenIdDoesNotExist();
+error ThisAccountDoesNotExist();
 error YouAreNotTheOwnerOfThisNFT();
+error YouAreNotAllowedToSeeThisData();
+error TheOwnerOfThisNftIsNotHisCreator();
+error CreateYourNftBeforeCallingThisFunction();
 
 contract MetaCareDynamicNFT is ERC721Enumerable, Ownable {
     using Strings for uint256;
@@ -15,14 +40,14 @@ contract MetaCareDynamicNFT is ERC721Enumerable, Ownable {
 
     //User Data structure (created during minting, values will be updated automatically)
     struct userData {
-        uint256 heartRate;
+        address userAddress;
         address doctorAddress;
-        // We can store any other data we want in the userData struct :
-        // userAddress, surname, name, age, weight, height and any other health data...
+        uint256 tokenId;
+        uint256 heartRate;
     }
 
     //create one structure of data for each user (we can find it using the NFT tokenID)
-    mapping(uint256 => userData) public userDataList;
+    mapping(address => userData) userDataList;
 
     //asign a name to the NFT collection
     constructor() ERC721("MetaCare", "MC") {}
@@ -33,65 +58,38 @@ contract MetaCareDynamicNFT is ERC721Enumerable, Ownable {
             revert YouAlreadyHaveAnNFT();
         }
 
-        //Get supply number / Add the user in the struct / Create the NFT
-        uint256 supply = totalSupply();
-        userDataList[supply + 1] = userData{80, _doctorAddress};
-        _safeMint(msg.sender, supply + 1);
+        //Add the user in the struct and create the NFT
+        uint256 tokenId = totalSupply() + 1
+        userDataList[msg.sender] = userData{msg.sender, _doctorAddress, tokenId , 80};
+        _safeMint(msg.sender, tokenId);
     }
-
-    //Get the Metadata for each NFTs
-    function tokenURI(uint256 _tokenId)
-        public
-        view
-        virtual
-        override
-        returns (string memory)
-    {
-        if(_exists(_tokenId) == false) {
-            revert ThisTokenIdDoesNotExist();
+    
+    //Give the possibility to change your doctor's address
+    function changeMyDoctorAddress(address _newDoctorAddress) external {
+        if (userDataList[msg.sender].userAddress != msg.sender) {
+           revert CreateYourNftBeforeCallingThisFunction();
         }
-
-        //Return the URI in function of the current heartRate
-        uint256 currentHeartRate = userDataList[_tokenId].heartRate;
-        if(currentHeartRate > 100){
-            return _highURI;
-        } else if (currentHeartRate < 60){
-            return _lowURI;
-        } else {
-            return _baseURI();
-        }
-    }
-
-    //define the Base URI
-    function _baseURI() internal view virtual override returns (string memory) {
-        return baseURI;
-    }
-
-
-    //give the possibility to change our doctor's address
-    function changeMyDoctorAddress(uint256 _tokenId, address _newDoctorAddress) external {
-        if (msg.sender != ownerOf(_tokenId)) {
-            revert YouAreNotTheOwnerOfThisNFT();
+        
+        if(msg.sender != ownerOf(userDataList[msg.sender].tokenId)){
+            revert TheOwnerOfThisNftIsNotHisCreator();
         }
 
         //change the doctor address
-        userDataList[_tokenId].doctorAddress = _newDoctorAddress;
+        userDataList[msg.sender].doctorAddress = _newDoctorAddress;
     }
 
-
-    //////////////////////////////////////////
-
-
+    //Enter your ETH address or the address of your patient
     function getUserData(address _userAddress)  
         external
         view
         returns (userData memory)
     {
-        //error if a database with this user address doesn't exist
-
         if (userDataList[_userAddress].userAddress != _userAddress) {
-            //it might be possible to simplify this if statement by just asking if userDataList[msg.sender] exists
-            revert MetaCare__ThisAccountDoesNotExist();
+            revert ThisAccountDoesNotExist();
+        }
+
+        if(_userAddress != ownerOf(userDataList[_userAddress].tokenId)){
+            revert TheOwnerOfThisNftIsNotHisCreator();
         }
 
         //check if the user want to see his own data
@@ -105,22 +103,50 @@ contract MetaCareDynamicNFT is ERC721Enumerable, Ownable {
         }
 
         //if the person calling the function isn't the data owner or a doctor, send him an error
-        revert MetaCare__YouAreNotAllowedToSeeThisData();
+        revert YouAreNotAllowedToSeeThisData();
     }
 
-
-    //function used by chainlink or the private data thing
+    //function used by chainlink to change the current heart Rate Data
     function changeUserHeartRateData(address _userAddress, uint256 _heartRate)
         external
-    {
-        //ADD AN IF STATEMENT HERE : Verify if the sender of this data is trusted
-
-        //error if a database with this user address doesn't exist
+    {   
         if (userDataList[_userAddress].userAddress != _userAddress) {
-            //it might be possible to simplify this if statement by just asking if userDataList[msg.sender] exists
-            revert MetaCare__ThisAccountDoesNotExist();
+            revert ThisAccountDoesNotExist();
         }
 
+        if(_userAddress != ownerOf(userDataList[_userAddress].tokenId)){
+            revert TheOwnerOfThisNftIsNotHisCreator();
+        }
+
+        //Change the heartRate data
         userDataList[_userAddress].heartRate = _heartRate;
+    }
+
+    //Get the Metadata for each NFTs
+    function tokenURI(uint256 _tokenId)
+        public
+        view
+        virtual
+        override
+        returns (string memory)
+    {
+        if(_exists(_tokenId) == false) {
+            revert ThisAccountDoesNotExist();
+        }
+
+        //Return the URI in function of the current heartRate
+        uint256 currentHeartRate = userDataList[_tokenId].heartRate;
+        if(currentHeartRate > 100){
+            return _highURI;
+        } else if (currentHeartRate < 60){
+            return _lowURI;
+        } else {
+            return _baseURI();
+        }
+    }
+
+    //Define the Base URI
+    function _baseURI() internal view virtual override returns (string memory) {
+        return baseURI;
     }
 }
